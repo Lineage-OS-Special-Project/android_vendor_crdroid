aaf0c4ab6cc8ab1cff6da152270d7a6fee7a8212
yehonatan5785 <yehonatan@mail.ru>
2026-07-10 13:21:29 +0100

    build: add configurable mobile services selection
    Introduce configurable mobile services selection to the LOSP
    device setup wizard.
    
    Users can now choose between Google Mobile Services (GMS),
    microG, or a vanilla build with no Google compatibility layer.
    
    The selected configuration is written to a generated
    .losp_build_config.mk file, allowing product composition to be
    configured independently of the device BoardConfig.
    
    Pixel Launcher selection is automatically restricted to GMS
    builds, while microG and vanilla builds default to the LOSP
    Special Launcher.
    
    Signed-off-by: yehonatan5785 <yehonatan@mail.ru>

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -7,9 +7,20 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 $(call inherit-product, vendor/lineage/config/telephony.mk)
 
-# GMS
-WITH_GMS ?= true
-ifeq ($(WITH_GMS),true)
+# -----------------------------------------------------------------------------
+# Mobile services provider
+# -----------------------------------------------------------------------------
+
+# Load the provider selected by the LOSP device setup wizard.
+-include $(TOPDIR).losp_build_config.mk
+
+# Default to full GMS when no local selection has been generated.
+LOSP_GMS_TYPE ?= gms
+
+ifeq ($(LOSP_GMS_TYPE),gms)
+
+WITH_GMS := true
+
 ifeq ($(TARGET_USES_MINI_GAPPS),true)
 $(call inherit-product, vendor/gms/gms_mini.mk)
 else ifeq ($(TARGET_USES_PICO_GAPPS),true)
@@ -17,4 +28,22 @@ $(call inherit-product, vendor/gms/gms_pico.mk)
 else
 $(call inherit-product, vendor/lineage/config/gms_full.mk)
 endif
+
+else ifeq ($(LOSP_GMS_TYPE),microg)
+
+WITH_GMS := false
+
+PRODUCT_PACKAGES += \
+    GmsCore \
+    GsfProxy \
+    FakeStore
+
+else ifeq ($(LOSP_GMS_TYPE),vanilla)
+
+WITH_GMS := false
+
+else
+
+$(error Invalid LOSP_GMS_TYPE "$(LOSP_GMS_TYPE)"; expected gms, microg, or vanilla)
+
 endif
